94fa8d2e4386676252567e54ad7449b182af205a
Pablo Noel <hello@pablonoel.com>
2016-05-20 22:48:26 -0400

    First Commit

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "assets/css/"
+sass_dir = "assets/sass/"
+images_dir = "assets/img/"
+javascripts_dir = "assets/js/"
+fonts_dir = "assets/fonts/"
+
+output_style = :compressed
+
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
+output_style = :compressed
+sass_options = {:debug_info=>true}
